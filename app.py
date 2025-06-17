92d5b356b66f66cdaec5ff2d86b23c0620fa6c19
Gabriel V Macaio <90156350+MegaUniverse@users.noreply.github.com>
2025-06-17 02:26:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,7 @@ from flask import Flask, request, jsonify, send_from_directory
 import os
 import requests
 
-ADZUNA_APP_ID = "9448da63"
-ADZUNA_APP_KEY = "7d4c3218999d914e87781ea7529bb1b3"
-
 app = Flask(__name__, static_folder="static")
-
-
-
 
 @app.route("/")
 def serve_index():
@@ -18,15 +12,15 @@ def serve_index():
 def serve_static(path):
     return send_from_directory(app.static_folder, path)
 
+# exemplo da sua API
 @app.route("/api/jobs")
 def get_jobs():
     query = request.args.get("query", "")
     page = request.args.get("page", "1")
-    url = f"https://api.adzuna.com/v1/api/jobs/br/search/{page}?app_id={ADZUNA_APP_ID}&app_key={ADZUNA_APP_KEY}&results_per_page=20&what={query}&content-type=application/json"
+    url = f"https://api.adzuna.com/v1/api/jobs/br/search/{page}?app_id=9448da63&app_key=7d4c3218999d914e87781ea7529bb1b3&results_per_page=20&what={query}&content-type=application/json"
     try:
         response = requests.get(url)
         data = response.json()
-
         results = []
         for job in data.get("results", []):
             results.append({
@@ -35,13 +29,10 @@ def get_jobs():
                 "location": job["location"]["display_name"],
                 "link": job["redirect_url"]
             })
-
         return jsonify({"results": results, "total": data.get("count", 200)})
-
     except Exception as e:
         return jsonify({"error": str(e)})
-import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # Render define essa PORT
-    app.run(host="0.0.0.0", port=port)         # Escuta na porta e host corretos
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
